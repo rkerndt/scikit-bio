fe591a5bbbe106a5ab5a05c2a065bbabb9856451
Rickie Kerndt <kerndtr@kerndt.com>
2016-05-22 09:07:49 -0700

    string method added for debugging

--- skbio/metadata/_feature.py
+++ skbio/metadata/_feature.py
@@ -1,7 +1,7 @@
 from skbio._base import SkbioObject
 from skbio.util._decorator import experimental
-
-
+from skbio._base import ElasticLines
+import textwrap
 
 
 
@@ -28,7 +28,23 @@ class Feature(SkbioObject):
     @experimental(as_of="0.4.2")
     def __str__(self):
         # TODO: string output for debugging/stdout display
-        pass
+        key_field = 10
+        value_field = 60
+        spaces = ' ' * key_field
+        type_format = ''.join(['{:',str(key_field),'s}{:',str(value_field),'}'])
+        fq0 = lambda k,v: spaces + k + '=' + v
+        fq1 = lambda k,v: type_format.format(k,v)
+        lines = ElasticLines()
+        lines.add_line(fq1(self.type_, self.location))
+        for key in sorted(self.qualifiers):
+            value = self.qualifiers[key]
+            if isinstance(value, list):
+                for v in value:
+                    lines.add_line(fq0(key, v))
+            else:
+                lines.add_line(fq0(key, value))
+        return lines.to_str()
+
 
     @experimental(as_of="0.4.2")
     def __repr__(self):
@@ -36,5 +52,25 @@ class Feature(SkbioObject):
         pass
 
 
+    def _wrap_text_with_indent(self, text, initial_text, extra_indent):
+        """Wrap text across lines with an initial indentation.
+
+        For example:
+
+            'foo': 'abc def
+                    ghi jkl
+                    mno pqr'
+
+        <indent>'foo':<space> is `initial_text`. `extra_indent` is 1. Wrapped
+        lines are indented such that they line up with the start of the
+        previous line of wrapped text.
+
+        TODO: taken from metadata/_repr.py.
+
+        """
+        return textwrap.wrap(
+            text, width=self._width, expand_tabs=False,
+            initial_indent=initial_text,
+            subsequent_indent=' ' * (len(initial_text) + extra_indent))
 
 
